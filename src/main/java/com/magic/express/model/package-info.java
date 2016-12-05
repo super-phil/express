84a185805b97c0be76210f3906a7544da01a053c
赵秀非 <zhaoxiufei@gmail.com>
2016-12-05 15:36:43 +0800

    init

--- src/main/java/com/magic/express/model/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * @author 赵秀非 E-mail:zhaoxiufei@gmail.com
- * @version 创建时间：2016/11/15 11:24
- * package-info
- */
-package com.magic.express.model;
--- src/main/resources/META-INF/resources/test.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Phil on 2016/11/16.
- */
-
-alert("xxxxxx");
