6a9c99a0e114eef5af9221c8b4a9b02d7896c29a
赵秀非 <zhaoxiufei@gmail.com>
2016-12-10 15:43:30 +0800

    zip 发送邮件

--- service/src/main/java/com/magic/express/config/ScheduleConfiguration.java
+++ service/src/main/java/com/magic/express/config/ScheduleConfiguration.java
@@ -2,9 +2,9 @@ package com.magic.express.config;
 
 import com.magic.express.service.EmailService;
 import com.magic.utils.database.DBUtils;
+import com.magic.utils.zip.ZipUtils;
 import org.apache.log4j.Logger;
 import org.joda.time.LocalDate;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -20,25 +20,27 @@ import java.io.File;
 @Configuration
 @EnableScheduling
 public class ScheduleConfiguration {
-    private static final Logger logger=Logger.getLogger(ScheduleConfiguration.class);
+    private static final Logger logger = Logger.getLogger(ScheduleConfiguration.class);
     @Resource
     private EmailService emailService;
-    
+
     /**
      * 每天8点备份数据库并发送邮件
      */
-    @Scheduled(cron="0 0 20 * * ?")
+    @Scheduled(cron = "0 0 20 * * ?")
     public void backupAndSendEmail() {
-        String backupPath="/root/phil/";
-        File file=new File(backupPath+"backup-"+LocalDate.now().toString("yyyy-MM-dd")+".jpg");
-        try{
-            String dbName="express";
-            String dbUser="root";
-            String dbPwd="1Q2w3e4r";
+        String backupPath = "/root/phil/";
+        File file = new File(backupPath + "backup-" + LocalDate.now().toString("yyyy-MM-dd") + ".jpg");
+        try {
+            String dbName = "express";
+            String dbUser = "root";
+            String dbPwd = "1Q2w3e4r";
             DBUtils.backup(file.getPath(), dbName, dbUser, dbPwd);
-            String email="717815@163.com";
-            emailService.sendAttachments(email, email, "express数据库备份", "数据库备份请查看附件!", file);
-        }catch(Exception e){
+            String zip = backupPath + "backup-" + LocalDate.now().toString("yyyy-MM-dd") + ".zip";
+            ZipUtils.zip(zip, file);
+            String email = "717815@163.com";
+            emailService.sendAttachments(email, email, "express数据库备份", "数据库备份请查看附件!", new File(zip));
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
