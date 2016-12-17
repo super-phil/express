5fdd85dc05440430d991871caf8d50497f11789c
赵秀非 <zhaoxiufei@gmail.com>
2016-12-17 21:53:43 +0800

    更新

--- service/src/main/java/com/magic/express/App.java
+++ service/src/main/java/com/magic/express/App.java
@@ -3,19 +3,41 @@ package com.magic.express;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.magic.utils.qiniu.QiNiuUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
 
 /**
  * Hello world!
  */
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
         System.out.println("Hello World!");
+    }
+
+    @RequestMapping(value = "test", method = RequestMethod.GET)
+    public Object list() {
+        String key = "IMG_20161114_153251.jpg";
+        return QiNiuUtils.del(key);
+    }
+
+    @RequestMapping(value = "apk", method = RequestMethod.GET)
+    public Object upApk() {
+        String key = "app-release.apk";
+        String file = "/root/phil/express/android/app/" + key;
+        QiNiuUtils.del(key);
+        return QiNiuUtils.upload(new File(file));
     }
 
     /**
@@ -29,7 +51,6 @@ public class App {
         FastJsonConfig fastJsonConfig = new FastJsonConfig();
         fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
         fastConverter.setFastJsonConfig(fastJsonConfig);
-        //        HttpMessageConverter<?> converter = fastConverter;
         return new HttpMessageConverters(fastConverter);
     }
 }
